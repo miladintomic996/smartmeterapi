e1eecdbfa55c2b841746d432faece8bc77b2301a
miladintomic996 <miladintomic996@gmail.com>
2021-05-19 18:37:49 +0200

    Added OpenApi and Swagger documentation

--- src/main/java/com/tq/smartmeterapi/SmartmeterapiApplication.java
+++ src/main/java/com/tq/smartmeterapi/SmartmeterapiApplication.java
@@ -1,10 +1,13 @@
 package com.tq.smartmeterapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Smart Meter API", version = "1.0", description = "API for managing smart meter data"))
 public class SmartmeterapiApplication {
 
     public static void main(String[] args) {
